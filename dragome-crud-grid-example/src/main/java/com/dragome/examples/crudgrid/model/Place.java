b96f348d1d1d70474f5b773f5107dcef5740df53
Fernando Petrola <fernando.petrola@dragome.com>
2014-08-31 19:18:43 -0300

    Making Place class comparable

--- dragome-crud-grid-example/src/main/java/com/dragome/examples/crudgrid/model/Place.java
+++ dragome-crud-grid-example/src/main/java/com/dragome/examples/crudgrid/model/Place.java
@@ -5,68 +5,73 @@ import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
 
 @Entity
-public class Place implements Identifiable
+public class Place implements Identifiable, Comparable<Place>
 {
-    @Id
-    @GeneratedValue
-    Integer id;
-    String name;
+	@Id
+	@GeneratedValue
+	Integer id;
+	String name;
 
-    public Place()
-    {
-    }
-
-    public Place(String name)
-    {
-	this.name= name;
-    }
-
-    public Integer getId()
-    {
-	return id;
-    }
-    public void setId(Integer id)
-    {
-	this.id= id;
-    }
-    public String getName()
-    {
-	return name;
-    }
-    public void setName(String name)
-    {
-	this.name= name;
-    }
-
-    public String toString()
-    {
-	return name + " ";
-    }
-    
-    public int hashCode()
-    {
-	final int prime= 31;
-	int result= 1;
-	result= prime * result + ((name == null) ? 0 : name.hashCode());
-	return result;
-    }
-
-    public boolean equals(Object obj)
-    {
-	if (this == obj)
-	    return true;
-	if (obj == null)
-	    return false;
-	if (getClass() != obj.getClass())
-	    return false;
-	Place other= (Place) obj;
-	if (name == null)
+	public Place()
 	{
-	    if (other.name != null)
-		return false;
 	}
-	else if (!name.equals(other.name))
-	    return false;
-	return true;
-    }
+
+	public Place(String name)
+	{
+		this.name= name;
+	}
+
+	public Integer getId()
+	{
+		return id;
+	}
+	public void setId(Integer id)
+	{
+		this.id= id;
+	}
+	public String getName()
+	{
+		return name;
+	}
+	public void setName(String name)
+	{
+		this.name= name;
+	}
+
+	public String toString()
+	{
+		return name + " ";
+	}
+
+	public int hashCode()
+	{
+		final int prime= 31;
+		int result= 1;
+		result= prime * result + ((name == null) ? 0 : name.hashCode());
+		return result;
+	}
+
+	public boolean equals(Object obj)
+	{
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Place other= (Place) obj;
+		if (name == null)
+		{
+			if (other.name != null)
+				return false;
+		}
+		else if (!name.equals(other.name))
+			return false;
+		return true;
+	}
+
+	public int compareTo(Place another)
+	{
+		return name.compareTo(another.name);
+	}
 }
