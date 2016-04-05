274545d0fe4c32b7ee5e56023b70994924a3ea50
Fernando Petrola <fernando.petrola@dragome.com>
2016-04-05 15:14:16 -0300

    fixing simple web example

--- dragome-minimal-web-example/src/main/java/com/dragome/example/DragomeConfiguration.java
+++ dragome-minimal-web-example/src/main/java/com/dragome/example/DragomeConfiguration.java
@@ -6,15 +6,16 @@ import com.dragome.web.config.DomHandlerApplicationConfigurator;
 @DragomeConfiguratorImplementor
 public class DragomeConfiguration extends DomHandlerApplicationConfigurator
 {
-    public boolean filterClassPath(String classpathEntry)
-    {
-	boolean include= false;
+	public boolean filterClassPath(String classpathEntry)
+	{
+		boolean include= false;
 
-	include|= classpathEntry.contains("dragome-js-jre-");
-	include|= classpathEntry.contains("dragome-js-commons-");
-	include|= classpathEntry.contains("dragome-core-");
-	include|= classpathEntry.contains("dragome-web-");
+		include|= classpathEntry.contains("dragome-js-jre-");
+		include|= classpathEntry.contains("dragome-js-commons-");
+		include|= classpathEntry.contains("dragome-core-");
+		include|= classpathEntry.contains("dragome-web-");
+		include|= classpathEntry.contains("dragome-w3c-standards");
 
-	return include;
-    }
+		return include;
+	}
 }
--- dragome-minimal-web-example/src/main/java/com/dragome/example/SimpleExample.java
+++ dragome-minimal-web-example/src/main/java/com/dragome/example/SimpleExample.java
@@ -6,29 +6,31 @@ import org.w3c.dom.Document;
 import org.w3c.dom.Element;
 import org.w3c.dom.events.Event;
 import org.w3c.dom.events.EventListener;
+import org.w3c.dom.events.EventTarget;
 
 import com.dragome.services.WebServiceLocator;
 import com.dragome.view.DefaultVisualActivity;
 import com.dragome.web.annotations.PageAlias;
-import com.dragome.web.dispatcher.EventDispatcherImpl;
+import com.dragome.web.enhancers.jsdelegate.JsCast;
 
 @PageAlias(alias= "hello-world")
 public class SimpleExample extends DefaultVisualActivity
 {
-    public void build()
-    {
-	Document document= WebServiceLocator.getInstance().getDomHandler().getDocument();
-
-	Element button= document.getElementById("button");
-	final Element messageElement= document.getElementById("message");
-
-	EventDispatcherImpl.setEventListener(button, new EventListener()
+	public void build()
 	{
-	    public void handleEvent(Event event)
-	    {
-		messageElement.setTextContent("hello world - " + new Date().toString());
-	    }
-	}, "click");
+		Document document= WebServiceLocator.getInstance().getDomHandler().getDocument();
 
-    }
+		Element button= document.getElementById("button");
+		final Element messageElement= document.getElementById("message");
+
+		EventTarget eventTarget= JsCast.castTo(button, EventTarget.class);
+
+		eventTarget.addEventListener("click", new EventListener()
+		{
+			public void handleEvent(Event event)
+			{
+				messageElement.setTextContent("hello world - " + new Date().toString());
+			}
+		}, false);
+	}
 }
